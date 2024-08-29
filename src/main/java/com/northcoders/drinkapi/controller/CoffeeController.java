f4b846c20ff10e8adaa01a824fa3380e6158c5cb
Qiujuan Wang <qiuqiu@qiujuans-mini.lan>
2024-08-29 15:42:14 +0100

    feat: add getCoffeeName in coffeeController

--- src/main/java/com/northcoders/drinkapi/controller/CoffeeController.java
+++ src/main/java/com/northcoders/drinkapi/controller/CoffeeController.java
@@ -1,5 +1,6 @@
 package com.northcoders.drinkapi.controller;
 
+import com.northcoders.drinkapi.model.Coffee;
 import com.northcoders.drinkapi.service.CoffeeService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,13 +9,14 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.atomic.AtomicLong;
 
 @RestController
 @RequestMapping("api/v1/coffee")
 
 public class CoffeeController {
+    AtomicLong counter;
     private final CoffeeService coffeeService;
-
     public CoffeeController(CoffeeService coffeeService) {
         this.coffeeService = coffeeService;
     }
@@ -27,5 +29,11 @@ public class CoffeeController {
     @GetMapping("/coffee-type")
     public Map<String, String> getCoffee(@RequestParam(value = "name", defaultValue = "latte") String name) {
         return coffeeService.getCoffeeDetails(name);
+    }
+
+    @GetMapping("/coffee-name")
+    public Coffee getCoffeeName(@RequestParam(value = "name", defaultValue = "latte") String name) {
+        return coffeeService.getCoffeeByName(name);
+
     }
 }
