fd139c06c4fd908c4b65cf05fb7c946534ac7ed8
Qiujuan Wang <qiuqiu@qiujuans-mini.lan>
2024-08-29 11:14:17 +0100

    feat: add test and controller for drinks

---
+++ src/main/java/com/northcoders/drinkapi/DrinkapiApplication.java
@@ -0,0 +1,13 @@
+package com.northcoders.drinkapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrinkapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DrinkapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/northcoders/drinkapi/DrinkapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.northcoders.drinkapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DrinkapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
